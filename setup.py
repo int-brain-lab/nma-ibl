8fad8806d97215bcb64816e63141a52a12fe0952
shenshan <shenshanpku@gmail.com>
2020-07-15 15:36:46 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='nma-ibl',
+    version='0.0.0',
+    description='Toolkits to access data in IBL public database',
+    author='Vathes',
+    author_email='support@vathes.com',
+    packages=find_packages(exclude=[]),
+    install_requires=['datajoint>=0.12', 'seaborn>=0.10.0'],
+)
